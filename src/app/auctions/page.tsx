c92b12365fa15a08ba61acd92c54d88024c222bc
Rafał Giemza <rafalgiemza@gmail.com>
2024-02-01 23:19:38 +0100

    move auctions to /auctions

---
+++ src/app/auctions/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { CreateAuction } from "~/components/main/auction/create-auction";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const auctions = await api.auction.getAll.query();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">      
+      <CreateAuction />
+
+      <h1>auctions:</h1>
+      {auctions.map(auction => 
+        <Link key={auction.id} href={`auction/${auction?.id}`}>{auction?.title}</Link>
+      )}
+
+    </main>
+  )
+}
