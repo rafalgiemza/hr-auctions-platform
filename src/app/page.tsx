3da0178969cfa8ef303aa0581d054ee9084cc7f1
Rafał Giemza <rafalgiemza@gmail.com>
2024-02-04 04:02:04 +0100

    update links on home/root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,79 @@
+import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+
+import { getServerAuthSession } from "~/server/auth";
+
 export default async function Home() {
-  return <main className=""></main>;
+  const session = await getServerAuthSession();
+  const links = [
+    {
+      url: "/dashboard",
+      label: "dashboard",
+      disabled: false,
+    },
+    {
+      url: "/mail",
+      label: "mail",
+      disabled: false,
+    },
+    {
+      url: "/music",
+      label: "music",
+      disabled: false,
+    },
+    {
+      url: "/auctions",
+      label: "AUCTIONS",
+      disabled: session === null,
+    },
+    {
+      url: "/profiles",
+      label: "PROFILES",
+      disabled: session === null,
+    },
+    {
+      url: "/T3",
+      label: "T3",
+      disabled: false,
+    },
+  ];
+
+  return (
+    <Table>
+      <TableCaption>Side map</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Link</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {links.map((item) => {
+          return (
+            <TableRow key={item.url}>
+              <TableCell className="font-medium">
+                {item.disabled ? (
+                  <p>You need to login to go to: {item.label}</p>
+                ) : (
+                  <Link
+                    href={item.url}
+                    className="rounded-full bg-white/10 px-3 py-3 font-semibold no-underline transition hover:bg-white/20 md:px-10"
+                  >
+                    {item.label}
+                  </Link>
+                )}
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
 }
